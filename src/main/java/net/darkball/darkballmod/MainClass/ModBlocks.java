ea16bd070aa4610a0d5ceaa60bbc257c1d41b5c4
darkball-002 <pp100qq20@gmail.com>
2024-10-16 11:09:46 +0800

    Add files via upload

---
+++ src/main/java/net/darkball/darkballmod/MainClass/ModBlocks.java
@@ -0,0 +1,58 @@
+package net.darkball.darkballmod.MainClass.block;
+
+import net.darkball.darkballmod.darkballmod;
+import net.darkball.darkballmod.MainClass.block.custom.*;
+import net.darkball.darkballmod.MainClass.item.*;
+import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+    DeferredRegister.create(ForgeRegistries.BLOCKS, darkballmod.MOD_ID);
+
+    public static final RegistryObject<Block> SAPPHIRE_BLOCK = registerBlock("sapphire_block",
+    		() -> new Block(BlockBehaviour.Properties.copy(Blocks.DIAMOND_BLOCK)));
+
+    
+    public static final RegistryObject<Block> SAPPHIRE_ORE = registerBlock("sapphire_ore",
+    	    () -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK)
+    				.strength(3f).requiresCorrectToolForDrops(), UniformInt.of(3, 7)));
+    
+    public static final RegistryObject<Block> DEEPSLATE_SAPPHIRE_ORE = registerBlock("deepslate_sapphire_ore",
+    		() -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.DIAMOND_BLOCK)
+    				.strength(3f).requiresCorrectToolForDrops(), UniformInt.of(3, 7)));
+
+    public static final RegistryObject<Block> BEDROCK_SAPPHIRE_ORE = registerBlock("bedrock_sapphire_ore",
+    		() -> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.NETHERITE_BLOCK)
+    				.strength(5f).requiresCorrectToolForDrops(), UniformInt.of(3, 7)));
+    
+    public static final RegistryObject<Block> GEM_POLISHING_STATION = registerBlock("gem_polishing_station",
+            () -> new GemPolishingStationBlock(BlockBehaviour.Properties.copy(Blocks.IRON_BLOCK).noOcclusion()));
+  
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+    	RegistryObject<T> toReturn = BLOCKS.register(name, block);
+    	registerBlockItem(name, toReturn);
+    	return toReturn;
+    }
+
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));}
+
+    public static void register(IEventBus eventBus){
+            BLOCKS.register(eventBus);
+        }
+    }
+
